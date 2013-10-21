7496f7141375038044f6d40512356039d4df6068
Nemestóthy Zsombor <nemzsom@gmail.com>
2013-10-21 08:48:15 +0200

    fractal drawing in simple form

--- drawers.rb
+++ drawers.rb
@@ -58,22 +58,44 @@ module Drawers
   class Fractal
   
     def initialize(window)
+      @window = window
+      @poligon = window.segments.clone
+      @iter = 0
     end
     
     def draw
-      
     end
     
     def button_down(id)
-
-    end
-    
-    def new_segment(p)
-      
     end
     
     def update
+      if @iter < 1000
+        new_poligon = []
+        @poligon.inject(nil) do |p1, segment|
+          p2  = mid_point(segment)
+          if p1
+            new_segment = [p1, p2]
+            @window.segments << new_segment
+            new_poligon << new_segment
+          end
+          p2
+        end
+        new_segment = [new_poligon.last[1], new_poligon.first[0]]
+        new_poligon << new_segment
+        @window.segments << new_segment
+        @poligon = new_poligon
+        @iter += 1
+      end      
       self
+    end
+    
+    def mid_point(segment)
+      p1 = segment[0]
+      p2 = segment[1]
+      mid_x = (p1[0] + p2[0]) / 2
+      mid_y = (p1[1] + p2[1]) / 2
+      [mid_x, mid_y]
     end
     
   end
